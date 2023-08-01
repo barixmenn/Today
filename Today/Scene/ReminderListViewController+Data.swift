5fcc26fb2339980e74b623260d5d62858565c010
baris dilekci <bdilekci200@gmail.com>
2023-08-01 21:34:55 +0300

    feat: button action done

--- Today/Scene/ReminderListViewController+Data.swift
+++ Today/Scene/ReminderListViewController+Data.swift
@@ -14,7 +14,7 @@ extension ReminderListViewController {
 
 
     func cellRegistrationHandler(cell: UICollectionViewListCell, indexPath: IndexPath, id: Reminder.ID) {
-        let reminder = Reminder.sampleData[indexPath.item]
+        let reminder = reminder(withId: id)
         var contentConfiguration = cell.defaultContentConfiguration()
         contentConfiguration.text = reminder.title
         contentConfiguration.secondaryText = reminder.dueDate.dayAndTimeText
@@ -54,8 +54,12 @@ extension ReminderListViewController {
           let symbolName = reminder.isComplete ? "circle.fill" : "circle"
           let symbolConfiguration = UIImage.SymbolConfiguration(textStyle: .title1)
           let image = UIImage(systemName: symbolName, withConfiguration: symbolConfiguration)
-          let button = UIButton()
+          let button = ReminderDoneButton()
+          button.addTarget(self, action: #selector(didPressDoneButton(_:)), for: .touchUpInside)
+          button.id = reminder.id
           button.setImage(image, for: .normal)
+          
+          
           return UICellAccessory.CustomViewConfiguration(
               customView: button, placement: .leading(displayed: .always))
       }
