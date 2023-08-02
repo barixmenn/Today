1cee1609f939f07f0b22dffeb385e090b75a68d3
baris dilekci <bdilekci200@gmail.com>
2023-08-02 16:12:48 +0300

    feat: edit page made

---
+++ Today/ContentView/UIContentConfiguration+Stateless.swift
@@ -0,0 +1,15 @@
+//
+//  UIContentConfiguration+Stateless..swift
+//  Today
+//
+//  Created by Baris on 2.08.2023.
+//
+
+import UIKit
+
+
+extension UIContentConfiguration {
+    func updated(for state: UIConfigurationState) -> Self {
+        return self
+    }
+}
